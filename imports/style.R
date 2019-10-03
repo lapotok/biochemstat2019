cb88095916e095578678a580fdd96c7b7ab9d3b9
lapotok <lapotok@gmail.com>
2019-10-03 12:33:43 +0300

    many changes

---
+++ imports/style.R
@@ -0,0 +1,13 @@
+knitr::opts_chunk$set(fig.align  = "center", 
+                      fig.width  = 4, 
+                      fig.height = 4, 
+                      echo       = T, 
+                      eval       = F, 
+                      comment    = "", 
+                      warning    = F,
+                      message    = F,
+                      cache      = T,
+                      dev        = "svg")
+options(
+  width = 100
+)
