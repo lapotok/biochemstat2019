5cb5dcc4d02c3011a91914a03fdfaf7f4f7ee457
lapotok <lapotok@gmail.com>
2019-10-08 15:41:53 +0300

    some new stuff

--- imports/style.R
+++ imports/style.R
@@ -6,8 +6,9 @@ knitr::opts_chunk$set(fig.align  = "center",
                       comment    = "", 
                       warning    = F,
                       message    = F,
-                      cache      = T,
+                      cache      = F,
                       dev        = "svg")
 options(
   width = 100
 )
+message('R style file was sourced.')
